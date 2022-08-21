50a3a74f7d1356372a0ce50c3cfbc5b798b8cf3a
ozrobert <oz.robert@o2.pl>
2022-08-21 12:34:29 +0200

    Change component App from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Throbber from './components/UI/Throbber';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
-import { Component } from 'react';
 import SearchBar from './components/UI/SearchBar/SearchBar';
 import Layout from './components/Layout/Layout';
 import Footer from './components/Footer/Footer';
@@ -11,96 +11,93 @@ import ThemeButton from './components/UI/ThemeButton/ThemeButton';
 import ThemeContext from './context/ThemeContext';
 import AuthContext from './context/AuthContext';
 
-class App extends Component {
-    hotels = [
-        {
-            id: 1,
-            name: 'Pod akacjami',
-            city: 'Warszawa',
-            rating: 8.3,
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione cumque a rem, eum nulla odit voluptates, deleniti labore sapiente nobis voluptatem voluptate sed eos nisi distinctio sint dolorem.',
-            image: '',
-        },
-        {
-            id: 2,
-            name: 'Dębowy',
-            city: 'Lublin',
-            rating: 8.9,
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione cumque a rem, eum nulla odit voluptates, deleniti labore sapiente nobis voluptatem voluptate sed eos nisi distinctio sint dolorem.',
-            image: '',
-        },
-    ];
+const backendHotels = [
+    {
+        id: 1,
+        name: 'Pod akacjami',
+        city: 'Warszawa',
+        rating: 8.3,
+        description:
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione cumque a rem, eum nulla odit voluptates, deleniti labore sapiente nobis voluptatem voluptate sed eos nisi distinctio sint dolorem.',
+        image: '',
+    },
+    {
+        id: 2,
+        name: 'Dębowy',
+        city: 'Lublin',
+        rating: 8.9,
+        description:
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione cumque a rem, eum nulla odit voluptates, deleniti labore sapiente nobis voluptatem voluptate sed eos nisi distinctio sint dolorem.',
+        image: '',
+    },
+];
 
-    state = {
-        hotels: [],
-        loading: true,
-        theme: 'primary',
-        isAuthenticated: false,
-    };
+function App() {
+    // *** INNY SPOSÓB ZAPISU STANU ***
+    // const [sate, setState] = useState({
+    //     hotels: [],
+    //     loading: true,
+    //     theme: 'primary',
+    //     isAuthenticated: false,
+    // });
 
-    searchHandler = (term) => {
-        const hotels = [...this.hotels].filter((hotel) =>
+    const [hotels, setHotels] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [theme, setTheme] = useState('primary');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const searchHandler = (term) => {
+        const newHotels = [...backendHotels].filter((hotel) =>
             hotel.name.toLowerCase().includes(term.trim().toLowerCase())
         );
-        this.setState({ hotels });
+        setHotels(newHotels);
     };
 
-    changeTheme = () => {
-        const newTheme = this.state.theme === 'primary' ? 'blue' : 'primary';
-        this.setState({ theme: newTheme });
+    const changeTheme = () => {
+        setTheme(theme === 'primary' ? 'blue' : 'primary');
     };
 
-    render() {
-        const header = (
-            <Header>
-                <SearchBar onSearch={this.searchHandler} />
-                <ThemeButton />
-            </Header>
-        );
-
-        const menu = <Menu auth />;
-
-        const content = this.state.loading ? (
-            <Throbber />
-        ) : (
-            <Hotels hotels={this.state.hotels} />
-        );
-
-        const footer = <Footer />;
-
-        return (
-            <AuthContext.Provider
-                value={{
-                    isAuthenticated: this.state.isAuthenticated,
-                    login: () => this.setState({ isAuthenticated: true }),
-                    logout: () => this.setState({ isAuthenticated: false }),
-                }}
-            >
-                <ThemeContext.Provider
-                    value={{
-                        color: this.state.theme,
-                        changeTheme: this.changeTheme,
-                    }}
-                >
-                    <Layout
-                        header={header}
-                        menu={menu}
-                        content={content}
-                        footer={footer}
-                    />
-                </ThemeContext.Provider>
-            </AuthContext.Provider>
-        );
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         // symulacja połączenia z backendem (opóźnienie otrzymania danych)
         setTimeout(() => {
-            this.setState({ hotels: this.hotels, loading: false });
+            setHotels(backendHotels);
+            setLoading(false);
         }, 1000);
-    }
+    }, []);
+
+    const header = (
+        <Header>
+            <SearchBar onSearch={searchHandler} />
+            <ThemeButton />
+        </Header>
+    );
+    const menu = <Menu />;
+    const content = loading ? <Throbber /> : <Hotels hotels={hotels} />;
+    const footer = <Footer />;
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isAuthenticated,
+                login: () => setIsAuthenticated(true),
+                logout: () => setIsAuthenticated(false),
+            }}
+        >
+            <ThemeContext.Provider
+                value={{
+                    color: theme,
+                    changeTheme,
+                }}
+            >
+                <Layout
+                    header={header}
+                    menu={menu}
+                    content={content}
+                    footer={footer}
+                />
+            </ThemeContext.Provider>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
